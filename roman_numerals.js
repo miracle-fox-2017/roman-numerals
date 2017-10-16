1bdb9787d68b0bc7f1ac7518e5d2f908e3e88ecd
trijadoel <triamri@gmail.com>
2017-10-16 18:20:30 +0700

    Tugas roman numeral

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,14 @@
 function to_roman (num) {
-  // your implementation code here
+  var arrNilai = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var arrAbjad = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+  var getHasil = '';
+  for (var i = 0; i <= arrNilai.length;i++){
+    while (num % arrNilai[i] < num){
+      getHasil += arrAbjad[i];
+      num -= arrNilai[i];
+    }
+  }
+  return getHasil;
 }
 
 // Drive code
