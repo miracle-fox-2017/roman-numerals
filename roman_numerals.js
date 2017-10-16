b4a7f9344b3e61621b5d40e0d70b26c3265d1b56
Tomy Djaya Budiman <tommybudi1998@gmail.com>
2017-10-16 16:56:21 +0700

    Roman Numerals

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,15 @@
-function to_roman (num) {
-  // your implementation code here
+function to_roman(num){
+    // // your implementation code here
+    var unqDecimal=[1000,900,500,400,100,90,50,40,10,9,5,4,1];
+    var unqRoman=["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+    var hasil="";
+    for(var i=0;i < unqDecimal.length;i++){
+        while(num >= unqDecimal[i]){
+            num-=unqDecimal[i]; // Param angka dikurangi unique decimal array.
+            hasil+=unqRoman[i]; // Hasil ditambahkan selama perulangan while berlangsung
+        }
+    }
+    return hasil;
 }
 
 // Drive code
