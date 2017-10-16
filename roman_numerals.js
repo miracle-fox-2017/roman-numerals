feff2e80fc2e38554b8ff34c326d8648e3dd1d2c
riskinputra <riski.n.putra@gmail.com>
2017-10-16 18:30:12 +0700

    riskinputra firstCommit

--- roman_numerals.js
+++ roman_numerals.js
@@ -1,5 +1,22 @@
 function to_roman (num) {
   // your implementation code here
+  /*
+  1. Dekalrasikan angka dengan romawinya;
+  2. Pisahkan angka kedalam golongan.
+  2. Dan untuk yang pola romawinya sama bisa menggunakan perulangan
+  */
+
+let arabic = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+let roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+let hasil = '';
+
+for(var i = 0; i < arabic.length; i++){
+  while(num >= arabic[i]){
+    num-=arabic[i];
+    hasil+= roman[i];
+  }
+}
+  return hasil;
 }
 
 // Drive code
